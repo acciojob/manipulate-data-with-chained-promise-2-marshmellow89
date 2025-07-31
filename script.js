e42b79e9cc4d586f91067bbb7f3ff27a51413386
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-31 22:41:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
-let arr = [1,2,3,4];
-let newArr = [];
-for(let i=0;i<arr.length;i++){
-	if(arr[i]%2==0){
-	newArr.push(arr[i]);
-	}
-};
+// let arr = [1,2,3,4];
+// let newArr = [];
+// for(let i=0;i<arr.length;i++){
+// 	if(arr[i]%2==0){
+// 	newArr.push(arr[i]);
+// 	}
+// };
 
-let newArr2 = [];
-		for(let i=0;i<newArr.length;i++){
-			newArr2.push(newArr[i]*2);
-		}
+// let newArr2 = [];
+// 		for(let i=0;i<newArr.length;i++){
+// 			newArr2.push(newArr[i]*2);
+// 		}
 
-let res1;
-let res2;
+// let res1;
+// let res2;
 			
-async function oneSD(){
- res1 = await new Promise(resolve => setTimeout(()=>resolve(newArr),1000));
-	console.log(res1);
+// async function oneSD(){
+//  res1 = await new Promise(resolve => setTimeout(()=>resolve(newArr),1000));
+// 	console.log(res1);
 	
- res2 = await new Promise(resolve => setTimeout(()=> resolve(newArr2),2000));
-   console.log(res2);
-}
+//  res2 = await new Promise(resolve => setTimeout(()=> resolve(newArr2),2000));
+//    console.log(res2);
+// }
 
-oneSD();
-document.getElementById("output").appendChild(res1,res2)
+// oneSD();
+// document.getElementById("output").appendChild(res1,res2)
+
+
+let arr = [1,2,3,4];
+async function arrfilter(){
+ await new Promise(resolve => setTimeout(() => {
+	 const NewArr = arr.filter(num => num%2==0);
+ },3000));
+
+const divTag = document.querySelector("#output");
+await new Promise(resolve => { setTimeout(() => {
+	divTag.appendChild(NewArr);
+}1000);
+	
+});
+
+NewArr2 = NewArr.map(num => num*2);
+	divTag.innerText ="";
+	await New Promise(resolve => {setTimeout(() => {
+		divTag.appendChild(NewArr2);
+	}2000);
+})
+};
